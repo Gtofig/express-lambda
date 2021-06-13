47576baf266ea1e7ef075e016d4c1be452846bf9
Tofig Hasanov <gtofig@yahoo.com>
2021-06-13 22:58:42 +0100

    Revert "Temporarily break service"
    This reverts commit f63c8b5cb7a50fb90ac1d96595a34d027988d514.

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.sendStatus(500);
+  res.send("Hello World!");
 });
 
 const port = 8080;
--- test/integ.test.js
+++ test/integ.test.js
@@ -5,7 +5,7 @@ test("Service returns 'Hello World!'", async () => {
     const response = await axios.get(process.env.SERVICE_ENDPOINT, {});
     expect(response.data).toBe("Hello World!");
   } catch (e) {
-    // console.error(e);
-    // throw e;
+    console.error(e);
+    throw e;
   }
 });
